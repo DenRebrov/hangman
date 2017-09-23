7dd8c66aaea3f6d6796388702937d46cf3d32e0c
Den Rebrov <den_rebrov@list.ru>
2017-09-23 14:33:02 +0300

    first version

---
+++ game.rb
@@ -0,0 +1,98 @@
+class Game
+  def initialize(slovo)
+    @letters = get_letters(slovo)
+
+    @errors = 0
+
+    @good_letters =[]
+    @bad_letters =[]
+
+    @status = 0
+  end
+
+  def status
+    @status
+  end
+
+  def get_letters(slovo)
+    if slovo == nil || slovo == ""
+      abort "Загадано пустое слово, нечего отгадывать. Закрываемся"
+    end
+
+    return slovo.encode('UTF-8').split("")
+  end
+
+  def ask_next_letter
+    letter = ""
+
+    while (letter == "") do
+      puts "Введите букву"
+      letter = STDIN.gets.encode("UTF-8").downcase.chomp
+    end
+    next_step(letter)
+  end
+
+  def next_step(bukva)
+    if @status == -1 || @status == 1
+      return
+    end
+
+    if @good_letters.include?(bukva) || @bad_letters.include?(bukva)
+      return
+    end
+
+    if @letters.include?(bukva) ||
+      (bukva == "е" && @letters.include?("ё")) ||
+      (bukva == "ё" && @letters.include?("е")) ||
+      (bukva == "и" && @letters.include?("й")) ||
+      (bukva == "й" && @letters.include?("и"))
+
+      @good_letters << bukva
+
+      if bukva == "е"
+        good_letters << "ё"
+      end
+
+      if bukva == "ё"
+        good_letters << "е"
+      end
+
+      if bukva == "и"
+        good_letters << "й"
+      end
+
+      if bukva == "й"
+        good_letters << "и"
+      end
+
+      if (@letters - @good_letters).empty?
+        @status = 1
+      else
+        @status = 0
+      end
+    else
+      @bad_letters << bukva
+      @errors += 1
+
+      if @errors >= 7
+        @status = -1
+      end
+    end
+  end
+
+  def letters
+    @letters
+  end
+
+  def good_letters
+    @good_letters
+  end
+
+  def bad_letters
+    @bad_letters
+  end
+
+  def errors
+    @errors
+  end
+end
---
+++ hangman_main.rb
@@ -0,0 +1,31 @@
+if Gem.win_platform?
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+
+require_relative "game"
+require_relative "result_printer"
+require_relative "word_reader"
+
+current_path = File.dirname(__FILE__)
+
+printer = ResultPrinter.new
+
+reader = WordReader.new
+
+words_file_name = current_path + "/data/random_word.txt"
+
+game = Game.new(reader.read_from_file(words_file_name))
+
+while game.status == 0 do
+  printer.print_status(game)
+  game.ask_next_letter
+end
+
+sleep 1
+
+printer.print_status(game)
